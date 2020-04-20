9e79325bb0adf0efd262361c3532d4ac0f1a6fa9
Jean Magno - Delta Se7en <jeanmagno77@yahoo.com.br>
2020-04-20 16:29:27 -0300

    'fileUploader(Partial)'

--- src/app/posts/post-create/post-create.component.ts
+++ src/app/posts/post-create/post-create.component.ts
@@ -1,9 +1,10 @@
 import { Component, OnInit } from "@angular/core";
-import { NgForm } from "@angular/forms";
+import { FormGroup, FormControl, Validators } from "@angular/forms";
 
 import { PostsService } from "../post.service";
 import { ActivatedRoute, ParamMap } from '@angular/router';
 import { Post } from '../post.model';
+import { mimeType } from './mime-type.validador'
 
 @Component({
   selector: "app-post-create",
@@ -14,7 +15,9 @@ export class PostCreateComponent implements OnInit {
   enteredTitle = "";
   enteredContent = "";
   post: Post;
+  form: FormGroup;
   isLoading = false;
+  imagePreview: string;
   private postId: string;
   private mode = "create";
 
@@ -25,6 +28,19 @@ export class PostCreateComponent implements OnInit {
   ) {}
 
   ngOnInit(){
+    this.form = new FormGroup({
+      title: new FormControl(
+          null,
+          {
+            validators: [Validators.required, Validators.minLength(3)]
+          }),
+      content: new FormControl(
+          null,
+          {
+            validators: [Validators.required]
+          }),
+      image: new FormControl(null, {validators: [Validators.required], asyncValidators: [mimeType]})
+    });
     this.route.paramMap.subscribe((paramMap: ParamMap) => {
       if(paramMap.has("postId")){
         this.mode = "edit";
@@ -34,6 +50,7 @@ export class PostCreateComponent implements OnInit {
         this.postsService.getPost(this.postId).subscribe(postData => {
           this.isLoading = false;
           this.post = { id: postData._id, title: postData.title, content: postData.content}
+          this.form.setValue({title: this.post.title, content: this.post.content});
         });
       }else{
         this.mode = "create";
@@ -42,20 +59,31 @@ export class PostCreateComponent implements OnInit {
     });
   }
 
-  onSavePost(form: NgForm){
-    if(form.invalid){
+  onImagePicked(event: Event){
+    const file = (event.target as HTMLInputElement).files[0];
+    this.form.patchValue({image: file});
+    this.form.get('image').updateValueAndValidity();
+    const reader = new FileReader();
+    reader.onload = () => {
+      this.imagePreview = reader.result as string;
+    };
+    reader.readAsDataURL(file);
+  }
+
+  onSavePost(){
+    if(this.form.invalid){
       return;
     }
     this.isLoading = true;
     if(this.mode === "create"){
-      this.postsService.addPost(form.value.title, form.value.content);
+      this.postsService.addPost(this.form.value.title, this.form.value.content, this.form.value.image);
     }else{
       this.postsService.updatePost(
         this.postId,
-        form.value.title,
-        form.value.content
+        this.form.value.title,
+        this.form.value.content
       );
     }
-    form.resetForm();
+    this.form.reset();
   }
 }
