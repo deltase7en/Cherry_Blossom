9969b7fe4ca366fe4b7bc08cd2017565177b1531
Jean Magno - Delta Se7en <jeanmagno77@yahoo.com.br>
2020-04-20 10:52:04 -0300

    ''

--- src/app/header/header.component.ts
+++ src/app/header/header.component.ts
@@ -2,6 +2,7 @@ import { Component } from "@angular/core";
 
 @Component({
   selector: 'app-header',
-  templateUrl: './head.component.html'
+  templateUrl: './head.component.html',
+  styleUrls: ['./header.component.css']
 })
 export class HeaderComponent {}
